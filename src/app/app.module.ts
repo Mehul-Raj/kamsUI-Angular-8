292bcee366299a585823b9160c9205d81ebbfce3
Mehul-Raj <mehulsaha@gmail.com>
2019-12-30 10:36:27 +0530

    With Header And Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MyComponent} from './mycomponent/mycomponent.component';
+import {Footer} from './component/component.footer';
+import { HeadderComponent } from './headder/headder.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    Footer,
+    HeadderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mycomponent/mycomponent.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+//Adding Own Component
+@Component({
+    selector :'app-component',
+    templateUrl:'./mycomponent.component.html'
+})
+
+export class MyComponent{
+
+}
